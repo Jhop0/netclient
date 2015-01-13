4f0a5c6cb3d3add5cb7779c721a7f7737f15db63
JR Hopkins <jhopkins32@gatech.edu>
2015-01-13 17:31:59 -0600

    2

--- client.c
+++ client.c
@@ -21,7 +21,7 @@ main (int argc, char *argv[])
    char buffer[256];
   if (argc < 3)
     {
-      fprintf (stderr, "usage %s hostname port\n", argv[0]);
+      fprintf (stderr, "usage %s hostnamE port\n", argv[0]);
       exit (0);
     }
   portno = atoi (argv[2]);
